f4f8986d97da4b23cad6612106fac2b0f624c21d
chinthaka417 <119521636+chinthaka417@users.noreply.github.com>
2023-06-27 11:27:47 +0530

    Practical 03.java
    Answers for practical 03

--- Practical 03.java
+++ Practical 03.java
@@ -1,4 +1,4 @@
-We have already discussed a about encapsulation while discussing OOPs concepts. 
+We have already discussed a about encapsulation while discussing OOPs concepts.  
 The whole idea behind encapsulation is to hide the implementation details from users. If a data member is private it means it can only be accessed within the same class. No outside class can access private data member (variable) of other class. However if we setup public getter and setter methods to update (for e.g. void setSSN(int ssn))and read (for e.g.  int getSSN()) the private data fields then the outside class can access those private data fields via public methods. This way data can only be accessed by public methods,  thus making the private fields and their implementation hidden for outside classes. That’s why encapsulation is known as data hiding.
 
 
